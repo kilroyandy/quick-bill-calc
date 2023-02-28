4ddb037b95d64a2d6d9466726840c717f82bf71b
Andrew Kilroy <kilroyandy@gmail.com>
2023-02-28 15:12:25 +0000

    autoupdate activated

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let rosiePercent = 0.37;
-let andrewPercent = 0.63;
-var rosieCut
-var andrewCut
-
-document.getElementById("submit").onclick = getValue;
+// document.getElementById("submit").onclick = getValue;
 
 function getValue() {
-  let value = document.getElementById("amount").value;
-  rosieCut = value * rosiePercent;
-  andrewCut = value * andrewPercent;
-  document.getElementById("andrew").innerHTML = "<p>Andrew to pay £" + andrewCut.toFixed(2) +"</p>";
-  document.getElementById("rosie").innerHTML = "<p>Rosie to pay £" + rosieCut.toFixed(2) + "</p>";
+	let rosiePercent = document.getElementById("rosiePercent").value / 100;
+	let andrewPercent = document.getElementById("andrewPercent").value / 100;
+	var rosieCut;
+	var andrewCut;
+	let value = document.getElementById("amount").value;
+	rosieCut = value * rosiePercent;
+	andrewCut = value * andrewPercent;
+	document.getElementById("andrew").innerHTML =
+		"<p>Andrew to pay £" + andrewCut.toFixed(2) + "</p>";
+	document.getElementById("rosie").innerHTML =
+		"<p>Rosie to pay £" + rosieCut.toFixed(2) + "</p>";
 }
-
-
